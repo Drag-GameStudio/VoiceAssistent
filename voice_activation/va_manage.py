746e76e093e3c2835391a8d7817c8b7c52ea69a2
dima-on <sinica911@gmail.com>
2026-01-29 02:44:18 +0200

    fix

--- voice_activation/va_manage.py
+++ voice_activation/va_manage.py
@@ -2,7 +2,7 @@ import pyaudio
 import numpy as np
 from .voice_activations_algorithms.base import BaseVAAlgorithm
 from singleton_models.py_audio_singleton import PyAudioManager
-
+import audioop
 
 
 class VAManager:
@@ -15,17 +15,18 @@ class VAManager:
         
         self.stream = PyAudioManager().py_audio.open(format=self.predict_algorithm.FORMAT,
                                                       channels=self.predict_algorithm.CHANNELS, 
-                                                      rate=self.predict_algorithm.RATE, 
+                                                      rate=48000, 
                                                       input=True, 
                                                       frames_per_buffer=self.predict_algorithm.CHUNK)
 
 
     def listen_micro(self):
         self.start_stream()
+        self.state = None
         while True:
-            data = self.stream.read(self.predict_algorithm.CHUNK)
-
-            activated = self.predict_algorithm.predict(data)
+            data = self.stream.read(1536, exception_on_overflow=False)
+            data_16k, self.state = audioop.ratecv(data, 2, 1, 48000, 16000, self.state)
+            activated = self.predict_algorithm.predict(data_16k)
             if activated:
                 print("activate")
                 break
